c2faf7f02ae624128d8c5622e8d0fc3ec8bc62a2
Vanitas <mi.abolencia@gmail.com>
2022-12-18 11:49:17 +0800

    errors via front

--- all_func.php
+++ all_func.php
@@ -5,7 +5,7 @@
 function getDatabaseConnection()  //Get connection from php to database
 {
     $host = 'localhost';
-    $dbname = 'house_rent_receipt';
+    $dbname = 'house_rental_db';
     $user = 'root';
     $password ='';
     $dsn = "mysql:dbname=$dbname;host=$host"; 
@@ -23,27 +23,67 @@ function getDatabaseConnection()  //Get connection from php to database
     }
 }
 
-function insert_data_create ($tnt_name, $house_num, $mm_rate, $date, $note) // For createpage
+function insert_data__renter ($tnt_id, $tnt_name, $cntct, $MOD, $mm) // RENTER
 {
     $conn = getDatabaseConnection(); 
-    $stmt = $conn->prepare("INSERT INTO house_rent VALUES (?, ?, ?, ?, ?)");
-    $stmt->execute([$tnt_name, $house_num, $mm_rate, $date, $note]); 
+    $stmt = $conn->prepare("INSERT INTO renter_info VALUES (?, ?, ?, ?, ?)");
+    $stmt->execute([$tnt_id, $tnt_name, $cntct, $MOD, $mm]); 
 }
 
-function insert_data_pay ($rec_id, $hs_id, $payday, $bal, $amnt_pay) // For PayPage
+function insert_data__house ($hs_id, $hs_type, $mm_rate, $add, $strt) //HOUSE
 {
     $conn = getDatabaseConnection(); 
-    $stmt = $conn->prepare("INSERT INTO receipt_table (?, ?, ?, ?, ?)");
-    $stmt->execute([$rec_id, $hs_id, $payday, $bal, $amnt_pay]); 
+    $stmt2 = $conn->prepare("INSERT INTO house_info values(?, ?, ?, ?, ?)");
+    $stmt2->execute([$hs_id, $hs_type, $mm_rate, $add, $strt]); 
 }
 
-function get_receipt($conn) //For receipt_page
+function insert_data__receipt ($rec_id, $bal, $ttl_bal, $date) //RECEIPT
 {
     $conn = getDatabaseConnection(); 
-    $stmt = $conn -> prepare("SELECT column_name(s)FROM table1 INNER JOIN table2 ON table1.column_name = table2.column_name;"); // Using inner join here for the receipt
-    $stmt -> execute();
-    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-    return $result;
+    $stmt = $conn->prepare("INSERT INTO receipt_table (Receipt_ID, Balance, Amount_paid, Date) values (?, ?, ?, ?, ?)");
+    $stmt->execute([$rec_id, $bal, $ttl_bal, $date]); 
 }
 
+// NOT SURE IF IM GONNA USE THIS THO :/
+// function get_receipt($conn) //For receipt_page
+// {
+//     $conn = getDatabaseConnection(); 
+//     $stmt = $conn -> prepare("SELECT column_name(s)FROM table1 INNER JOIN table2 ON table1.column_name = table2.column_name;"); // Using inner join here for the receipt
+//     $stmt -> execute();
+//     $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+//     return $result;
+// }
+
+function generateRandomString($length = 10) // GENERATE RAND STRING
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+
+// FINAL USAGE
+// include_once 'all_func.php';
+
+// $lgnth = 10;
+// $hs_id = generateRandomString($lgnth);
+
+// $bal = rand(1000000000, 1000000000000000);
+// $total_bal = $bal * $_POST['months__to__stay'];
+// $date = date('y-m-d');
+
+
+// if (isset($_POST['Submit']))
+// {
+// insert_data__renter($_POST['tenant__id'],$_POST['tenant__name'],$_POST['contact__number'],$_POST['mode__of__payment'],$_POST['months__to__stay']); // check
+
+// insert_data__house($hs_id,$_POST['home__types'],$_POST['mothly__rent'],$_POST['address'],$_POST['street']); // check
+
+// insert_data__receipt(['generate__receipt__no'], $bal, $total_bal, $date); // on process 
+// header('test.php');
+// }
 ?>
--- js/main.js
+++ js/main.js
@@ -10,21 +10,51 @@ const initApp = () => {
   const inputGenerateRecNo = document.querySelector("#generate__receipt__no");
   const selectHomeTypes = document.querySelector("#home__types");
   const monthlyRent = document.querySelector("#monthly__rent");
+  const getCurrentYear = document.getElementById("getCurrentYear");
+  const overlay = document.querySelector(".overlay");
+  const submitBtn = document.querySelector("#submit");
+  const backBtn = document.querySelector("#back_btn");
+  const printBtn = document.querySelector("#print__btn");
   let randomNum;
 
+  const d = new Date();
+  getCurrentYear.innerHTML = d.getFullYear();
   const generateRandomNum = () => {
     randomNum = Math.floor(Math.random() * 99999) + 10000;
     return randomNum;
   };
 
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (overlay.classList.contains("isActive")) {
+      overlay.classList.remove("isActive");
+    } else {
+      overlay.classList.add("isActive");
+    }
+  });
+
+  backBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (overlay.classList.contains("isActive")) {
+      overlay.classList.remove("isActive");
+    } else {
+      overlay.classList.add("isActive");
+    }
+  });
+
   generateRecNoBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    e.stopPropagation();
 
     inputGenerateRecNo.value = generateRandomNum();
   });
 
   selectHomeTypes.addEventListener("change", () => {
     monthlyRent.value = selectHomeTypes.value;
+  });
+
+  printBtn.addEventListener("click", (e) => {
+    window.print();
   });
 };
